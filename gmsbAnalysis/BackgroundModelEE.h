3d26a4425075af6655b8b356dc7d8f3b5b46a8d5
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2010-11-20 18:18:25 +0000

    start putting things in analysis code

--- gmsbAnalysis/BackgroundModelEE.h
+++ gmsbAnalysis/BackgroundModelEE.h
@@ -2,13 +2,37 @@
 #define GMSBANALYSIS_BACKGROUNDMODELEE_H
 
 #include "AthenaBaseComps/AthAlgorithm.h"
+#include "GaudiKernel/ITHistSvc.h"
+
 /////////////////////////////////////////////////////////////////////////////
 class BackgroundModelEE:public AthAlgorithm {
-    public:
-    BackgroundModelEE (const std::string& name, ISvcLocator* pSvcLocator);
-    StatusCode initialize();
-    StatusCode execute();
-    StatusCode finalize();
+public:
+  BackgroundModelEE (const std::string& name, ISvcLocator* pSvcLocator);
+  StatusCode initialize();
+  StatusCode execute();
+  StatusCode finalize();
+
+private:
+
+  /** Electron selection */
+  std::string m_ElectronContainerName;
+  int m_electronIsEM;
+
+  /** Photon selection */
+  std::string m_PhotonContainerName;
+  int m_photonIsEM;
+
+  /** MET selecton */
+  std::string m_METContainerName;
+
+  std::string m_JetContainerName;
+
+  /// a handle on the Hist/TTree registration service
+  ITHistSvc * m_thistSvc;
+  std::string m_histFileName;
+
+  std::map<std::string, TH1*> m_histograms;
+  
 };
 
 #endif // GMSBANALYSIS_BACKGROUNDMODELEE_H
--- src/BackgroundModelEE.cxx
+++ src/BackgroundModelEE.cxx
@@ -1,25 +1,64 @@
 #include "gmsbAnalysis/BackgroundModelEE.h"
-//#include "GaudiKernel/MsgStream.h"
+
+#include "TH1.h"
+
+#include "egammaEvent/ElectronContainer.h"
+#include "egammaEvent/Electron.h"
+#include "egammaEvent/PhotonContainer.h"
+#include "egammaEvent/Photon.h"
+#include "egammaEvent/egammaPIDdefs.h"
+
+#include "JetEvent/JetCollection.h"
+
+#include "MissingETEvent/MissingET.h"
+
 /////////////////////////////////////////////////////////////////////////////
 BackgroundModelEE::BackgroundModelEE(const std::string& name, ISvcLocator* pSvcLocator) :
-AthAlgorithm(name, pSvcLocator)
+  AthAlgorithm(name, pSvcLocator)
 {
-  // Properties go here
+  declareProperty("HistFileName", m_histFileName = "BackgroundModelEE");
+
+  /** Electron selection */
+  declareProperty("ElectronContainerName", m_ElectronContainerName = "ElectronAODCollection");
+  declareProperty("ElectronIsEM", m_electronIsEM = egammaPID::ElectronMedium_WithTrackMatch);
+
+  declareProperty("PhotonContainerName", m_PhotonContainerName = "PhotonAODCollection");
+  declareProperty("PhotonIsEM", m_photonIsEM = egammaPID::PhotonTight);
+
+  declareProperty("METContainerName", m_METContainerName = "MET_LocHadTopo");
+  declareProperty("JetContainerName", m_JetContainerName = "AntiKt4TopoJets");
+
 }
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 StatusCode BackgroundModelEE::initialize(){
 
-    ATH_MSG_INFO ("initialize()");
-
-    return StatusCode::SUCCESS;
+  ATH_MSG_DEBUG("initialize()");
+  
+  /// histogram location
+  StatusCode sc = service("THistSvc", m_thistSvc);
+  if(sc.isFailure()) {
+    ATH_MSG_ERROR("Unable to retrieve pointer to THistSvc");
+    return sc;
+  }
+	
+  return StatusCode::SUCCESS;
 }
+
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-StatusCode BackgroundModelEE::execute() {
-
-   ATH_MSG_INFO ("Your new package and algorithm are successfully executing");
-
-    return StatusCode::SUCCESS;
+StatusCode BackgroundModelEE::execute() 
+{
+  ATH_MSG_DEBUG("execute");
+  
+  const JetCollection* jetColl = 0;
+  StatusCode sc = evtStore()->retrieve( jetColl, m_JetContainerName);
+  if( sc.isFailure()  ||  !jetColl ) {
+    ATH_MSG_ERROR("No Jet Collection  "<< m_JetContainerName);
+    return StatusCode::RECOVERABLE;
+  }
+  
+  return StatusCode::SUCCESS;
 }
+
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 StatusCode BackgroundModelEE::finalize() {
     
