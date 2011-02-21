7adde294a792a9143ab184b966be43703ec9605a
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2011-02-21 23:20:25 +0000

    accumulate uncertainty

---
+++ gmsbAnalysis/AccumulateUncert.h
@@ -0,0 +1,15 @@
+#ifndef GMSBANALYSIS_ACCUMULATEUNCERT_H
+#define GMSBANALYSIS_ACCUMULATEUNCERT_H
+
+class AcculateUncert {
+public:
+  AccumlateUncert() : m_num(0), m_denom(0) {};
+  double Uncert() const {return m_denom ? m_num/m_denom : 0;};
+  void AddObject(double pt, bool isBarrel, bool isConv, double weight);
+
+private:
+  double m_num;
+  double m_denom;
+};
+
+#endif
---
+++ src/AccumulateUncert.cxx
@@ -0,0 +1,84 @@
+#include "gmsbAnalysis/AccumulateUncert.h"
+
+AcculateUncert::AddObject(double pt, bool isBarrel, bool isConv, double weight)
+{
+  m_denom += weight;
+  if (isConv) {
+    if (isBarrel) {
+      if (pt < 20000) {
+	m_num += weight * 7.7;
+      } else if (pt < 25000) {
+	m_num += weight * 5.4;
+      } else if (pt < 30000) {
+	m_num += weight * 4.7;
+      } else if (pt < 35000) {
+	m_num += weight * 4.0;
+      } else if (pt < 40000) {
+	m_num += weight * 4.3;
+      } else if (pt < 50000) {
+	m_num += weight * 3.6;
+      } else if (pt < 60000) {
+	m_num += weight * 1;
+      } else {
+	m_num += weight * 3;
+      }
+    } else {
+      if (pt < 20000) {
+	m_num += weight * 7.6;
+      } else if (pt < 25000) {
+	m_num += weight * 8.1;
+      } else if (pt < 30000) {
+	m_num += weight * 6.9;
+      } else if (pt < 35000) {
+	m_num += weight * 6.0;
+      } else if (pt < 40000) {
+	m_num += weight * 5.5;
+      } else if (pt < 50000) {
+	m_num += weight * 5.3;
+      } else if (pt < 60000) {
+	m_num += weight * 4;
+      } else {
+	m_num += weight * 5;
+      }
+    }
+  } else {
+    if (isBarrel) {
+      if (pt < 20000) {
+	m_num += weight * 4.1;
+      } else if (pt < 25000) {
+	m_num += weight * 3.6;
+      } else if (pt < 30000) {
+	m_num += weight * 2.7;
+      } else if (pt < 35000) {
+	m_num += weight * 2.0;
+      } else if (pt < 40000) {
+	m_num += weight * 1.7;
+      } else if (pt < 50000) {
+	m_num += weight * 1.3;
+      } else if (pt < 60000) {
+	m_num += weight * 0.3;
+      } else {
+	m_num += weight * 0.6;
+      }
+    } else {
+      if (pt < 20000) {
+	m_num += weight * 3.9;
+      } else if (pt < 25000) {
+	m_num += weight * 2.6;
+      } else if (pt < 30000) {
+	m_num += weight * 1.5;
+      } else if (pt < 35000) {
+	m_num += weight * 1.2;
+      } else if (pt < 40000) {
+	m_num += weight * 0;
+      } else if (pt < 50000) {
+	m_num += weight * 0.6;
+      } else if (pt < 60000) {
+	m_num += weight * 2;
+      } else {
+	m_num += weight * 0;
+      }
+    }
+  }
+}
+		 
