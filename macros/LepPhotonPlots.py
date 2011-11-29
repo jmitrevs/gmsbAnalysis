9565123672f69702eb1c37e712a9f293dd202c61
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2011-11-29 18:31:01 +0000

    first working version of macros

--- macros/LepPhotonPlots.py
+++ macros/LepPhotonPlots.py
@@ -239,49 +239,49 @@ def LepPhotonPlots(lepton):
 
     # let's print out the number of events for debugging
     print "Number of input events:"
-    print "\tnOrigWlepnu_Np0 = ", nOrigWlepnu_Np0
-    print "\tnOrigWlepnu_Np1 = ", nOrigWlepnu_Np1
-    print "\tnOrigWlepnu_Np2 = ", nOrigWlepnu_Np2
-    print "\tnOrigWlepnu_Np3 = ", nOrigWlepnu_Np3
-    print "\tnOrigWlepnu_Np4 = ", nOrigWlepnu_Np4
-    print "\tnOrigWlepnu_Np5 = ", nOrigWlepnu_Np5
+    print "\tnOrigWlepnu_Np0 =", nOrigWlepnu_Np0
+    print "\tnOrigWlepnu_Np1 =", nOrigWlepnu_Np1
+    print "\tnOrigWlepnu_Np2 =", nOrigWlepnu_Np2
+    print "\tnOrigWlepnu_Np3 =", nOrigWlepnu_Np3
+    print "\tnOrigWlepnu_Np4 =", nOrigWlepnu_Np4
+    print "\tnOrigWlepnu_Np5 =", nOrigWlepnu_Np5
     
-    print "\tnOrigWtaunu_Np0 = ", nOrigWtaunu_Np0
-    print "\tnOrigWtaunu_Np1 = ", nOrigWtaunu_Np1
-    print "\tnOrigWtaunu_Np2 = ", nOrigWtaunu_Np2
-    print "\tnOrigWtaunu_Np3 = ", nOrigWtaunu_Np3
-    print "\tnOrigWtaunu_Np4 = ", nOrigWtaunu_Np4
-    print "\tnOrigWtaunu_Np5 = ", nOrigWtaunu_Np5
+    print "\tnOrigWtaunu_Np0 =", nOrigWtaunu_Np0
+    print "\tnOrigWtaunu_Np1 =", nOrigWtaunu_Np1
+    print "\tnOrigWtaunu_Np2 =", nOrigWtaunu_Np2
+    print "\tnOrigWtaunu_Np3 =", nOrigWtaunu_Np3
+    print "\tnOrigWtaunu_Np4 =", nOrigWtaunu_Np4
+    print "\tnOrigWtaunu_Np5 =", nOrigWtaunu_Np5
     
-    print "\tnOrigWgamma_Np0 = ", nOrigWgamma_Np0
-    print "\tnOrigWgamma_Np1 = ", nOrigWgamma_Np1
-    print "\tnOrigWgamma_Np2 = ", nOrigWgamma_Np2
-    print "\tnOrigWgamma_Np3 = ", nOrigWgamma_Np3
-    print "\tnOrigWgamma_Np4 = ", nOrigWgamma_Np4
-    print "\tnOrigWgamma_Np5 = ", nOrigWgamma_Np5
+    print "\tnOrigWgamma_Np0 =", nOrigWgamma_Np0
+    print "\tnOrigWgamma_Np1 =", nOrigWgamma_Np1
+    print "\tnOrigWgamma_Np2 =", nOrigWgamma_Np2
+    print "\tnOrigWgamma_Np3 =", nOrigWgamma_Np3
+    print "\tnOrigWgamma_Np4 =", nOrigWgamma_Np4
+    print "\tnOrigWgamma_Np5 =", nOrigWgamma_Np5
     
-    print "\tnOrigttbar = ", nOrigttbar
+    print "\tnOrigttbar =", nOrigttbar
     
-    print "\tnOrigst_tchan_lepnu = ", nOrigst_tchan_lepnu
-    print "\tnOrigst_tchan_taunu = ", nOrigst_tchan_taunu
+    print "\tnOrigst_tchan_lepnu =", nOrigst_tchan_lepnu
+    print "\tnOrigst_tchan_taunu =", nOrigst_tchan_taunu
     
-    print "\tnOrigst_schan_lepnu = ", nOrigst_schan_lepnu
-    print "\tnOrigst_schan_taunu = ", nOrigst_schan_taunu
+    print "\tnOrigst_schan_lepnu =", nOrigst_schan_lepnu
+    print "\tnOrigst_schan_taunu =", nOrigst_schan_taunu
     
-    print "\tnOrigst_Wt = ", nOrigst_Wt
+    print "\tnOrigst_Wt =", nOrigst_Wt
 
-    print "\tnOrigWW = ", nOrigWW
-    print "\tnOrigWZ = ", nOrigWZ
-    print "\tnOrigZZ = ", nOrigZZ
+    print "\tnOrigWW =", nOrigWW
+    print "\tnOrigWZ =", nOrigWZ
+    print "\tnOrigZZ =", nOrigZZ
 
-    print "\tnOrigZleplepgamma = ", nOrigZleplepgamma
-    print "\tnOrigZtautaugamma = ", nOrigZtautaugamma
+    print "\tnOrigZleplepgamma =", nOrigZleplepgamma
+    print "\tnOrigZtautaugamma =", nOrigZtautaugamma
 
-    print "\tnOriggamma_Np1 = ", nOriggamma_Np1
-    print "\tnOriggamma_Np2 = ", nOriggamma_Np2
-    print "\tnOriggamma_Np3 = ", nOriggamma_Np3
-    print "\tnOriggamma_Np4 = ", nOriggamma_Np4
-    print "\tnOriggamma_Np5 = ", nOriggamma_Np5
+    print "\tnOriggamma_Np1 =", nOriggamma_Np1
+    print "\tnOriggamma_Np2 =", nOriggamma_Np2
+    print "\tnOriggamma_Np3 =", nOriggamma_Np3
+    print "\tnOriggamma_Np4 =", nOriggamma_Np4
+    print "\tnOriggamma_Np5 =", nOriggamma_Np5
 
 
     ##############################################
@@ -356,137 +356,173 @@ def LepPhotonPlots(lepton):
     #   Take the ttbar as the input that defines what should be in every file
     histNames = GetHistNames(ttbarFile)
     
-    print histNames
+    #print histNames
 
-#   ////////////////////////////
+    ##########################################################
 
-#   Wlepnu_Np0->Scale(Wlepnu_Np0_scale)
-#   Wlepnu_Np1->Scale(Wlepnu_Np1_scale)
-#   Wlepnu_Np2->Scale(Wlepnu_Np2_scale)
-#   Wlepnu_Np3->Scale(Wlepnu_Np3_scale)
-#   Wlepnu_Np4->Scale(Wlepnu_Np4_scale)
-#   Wlepnu_Np5->Scale(Wlepnu_Np5_scale)
+    for histName in histNames:
+        
+        Wlepnu_Np0 = WlepnuFile_Np0.Get(histName)
+        Wlepnu_Np1 = WlepnuFile_Np1.Get(histName)
+        Wlepnu_Np2 = WlepnuFile_Np2.Get(histName)
+        Wlepnu_Np3 = WlepnuFile_Np3.Get(histName)
+        Wlepnu_Np4 = WlepnuFile_Np4.Get(histName)
+        Wlepnu_Np5 = WlepnuFile_Np5.Get(histName)
 
-#   Wmunu_Np0->Scale(Wmunu_Np0_scale)
-#   Wmunu_Np1->Scale(Wmunu_Np1_scale)
-#   Wmunu_Np2->Scale(Wmunu_Np2_scale)
-#   Wmunu_Np3->Scale(Wmunu_Np3_scale)
-#   Wmunu_Np4->Scale(Wmunu_Np4_scale)
-#   Wmunu_Np5->Scale(Wmunu_Np5_scale)
+        Wtaunu_Np0 = WtaunuFile_Np0.Get(histName)
+        Wtaunu_Np1 = WtaunuFile_Np1.Get(histName)
+        Wtaunu_Np2 = WtaunuFile_Np2.Get(histName)
+        Wtaunu_Np3 = WtaunuFile_Np3.Get(histName)
+        Wtaunu_Np4 = WtaunuFile_Np4.Get(histName)
+        Wtaunu_Np5 = WtaunuFile_Np5.Get(histName)
+        
+        Wgamma_Np0 = WgammaFile_Np0.Get(histName)
+        Wgamma_Np1 = WgammaFile_Np1.Get(histName)
+        Wgamma_Np2 = WgammaFile_Np2.Get(histName)
+        Wgamma_Np3 = WgammaFile_Np3.Get(histName)
+        Wgamma_Np4 = WgammaFile_Np4.Get(histName)
+        Wgamma_Np5 = WgammaFile_Np5.Get(histName)
+        
+        ttbar = ttbarFile.Get(histName)
+        
+        st_tchan_lepnu = st_tchan_lepnuFile.Get(histName)
+        st_tchan_taunu = st_tchan_taunuFile.Get(histName)
+        
+        st_schan_lepnu = st_schan_lepnuFile.Get(histName)
+        st_schan_taunu = st_schan_taunuFile.Get(histName)
+        
+        st_Wt   = st_WtFile.Get(histName)
+        
+        WW   = WWFile.Get(histName)
+        WZ   = WZFile.Get(histName)
+        ZZ   = ZZFile.Get(histName)
+        
+        Zleplepgamma = ZleplepgammaFile.Get(histName)
+        Ztautaugamma = ZtautaugammaFile.Get(histName)
+        
+        gamma_Np1 = gammaFile_Np1.Get(histName)
+        gamma_Np2 = gammaFile_Np2.Get(histName)
+        gamma_Np3 = gammaFile_Np3.Get(histName)
+        gamma_Np4 = gammaFile_Np4.Get(histName)
+        gamma_Np5 = gammaFile_Np5.Get(histName)
 
-#   Wtaunu_Np0->Scale(Wtaunu_Np0_scale)
-#   Wtaunu_Np1->Scale(Wtaunu_Np1_scale)
-#   Wtaunu_Np2->Scale(Wtaunu_Np2_scale)
-#   Wtaunu_Np3->Scale(Wtaunu_Np3_scale)
-#   Wtaunu_Np4->Scale(Wtaunu_Np4_scale)
-#   Wtaunu_Np5->Scale(Wtaunu_Np5_scale)
+        #######################################
 
-#   Wgamma_Np0->Scale(Wgamma_Np0_scale)
-#   Wgamma_Np1->Scale(Wgamma_Np1_scale)
-#   Wgamma_Np2->Scale(Wgamma_Np2_scale)
-#   Wgamma_Np3->Scale(Wgamma_Np3_scale)
-#   Wgamma_Np4->Scale(Wgamma_Np4_scale)
-#   Wgamma_Np5->Scale(Wgamma_Np5_scale)
+        Wlepnu_Np0.Scale(Wlepnu_Np0_scale)
+        Wlepnu_Np1.Scale(Wlepnu_Np1_scale)
+        Wlepnu_Np2.Scale(Wlepnu_Np2_scale)
+        Wlepnu_Np3.Scale(Wlepnu_Np3_scale)
+        Wlepnu_Np4.Scale(Wlepnu_Np4_scale)
+        Wlepnu_Np5.Scale(Wlepnu_Np5_scale)
 
-#   ttbar->Scale(ttbar_scale)
+        Wtaunu_Np0.Scale(Wtaunu_Np0_scale)
+        Wtaunu_Np1.Scale(Wtaunu_Np1_scale)
+        Wtaunu_Np2.Scale(Wtaunu_Np2_scale)
+        Wtaunu_Np3.Scale(Wtaunu_Np3_scale)
+        Wtaunu_Np4.Scale(Wtaunu_Np4_scale)
+        Wtaunu_Np5.Scale(Wtaunu_Np5_scale)
+        
+        Wgamma_Np0.Scale(Wgamma_Np0_scale)
+        Wgamma_Np1.Scale(Wgamma_Np1_scale)
+        Wgamma_Np2.Scale(Wgamma_Np2_scale)
+        Wgamma_Np3.Scale(Wgamma_Np3_scale)
+        Wgamma_Np4.Scale(Wgamma_Np4_scale)
+        Wgamma_Np5.Scale(Wgamma_Np5_scale)
+        
+        ttbar.Scale(ttbar_scale)
+        
+        st_tchan_lepnu.Scale(st_tchan_lepnu_scale)
+        st_tchan_taunu.Scale(st_tchan_taunu_scale)
+        
+        st_schan_lepnu.Scale(st_schan_lepnu_scale)
+        st_schan_taunu.Scale(st_schan_taunu_scale)
 
-#   st_tchan_lepnu->Scale(st_tchan_lepnu_scale)
-#   st_tchan_munu->Scale(st_tchan_munu_scale)
-#   st_tchan_taunu->Scale(st_tchan_taunu_scale)
+        st_Wt.Scale(st_Wt_scale)
 
-#   st_schan_lepnu->Scale(st_schan_lepnu_scale)
-#   st_schan_munu->Scale(st_schan_munu_scale)
-#   st_schan_taunu->Scale(st_schan_taunu_scale)
+        WW.Scale(WW_scale)
+        WZ.Scale(WZ_scale)
+        ZZ.Scale(ZZ_scale)
+        
+        Zleplepgamma.Scale(Zleplepgamma_scale)
+        Ztautaugamma.Scale(Ztautaugamma_scale)
+        
+        gamma_Np1.Scale(gamma_Np1_scale)
+        gamma_Np2.Scale(gamma_Np2_scale)
+        gamma_Np3.Scale(gamma_Np3_scale)
+        gamma_Np4.Scale(gamma_Np4_scale)
+        gamma_Np5.Scale(gamma_Np5_scale)
 
-#   st_Wt->Scale(st_Wt_scale)
+        ############################################
 
-#   ////////////////////
+        Wlepnu = Wlepnu_Np0.Clone()
+        Wlepnu.Add(Wlepnu_Np1)
+        Wlepnu.Add(Wlepnu_Np2)
+        Wlepnu.Add(Wlepnu_Np3)
+        Wlepnu.Add(Wlepnu_Np4)
+        Wlepnu.Add(Wlepnu_Np5)
 
-#   TH1F* Wlepnu = Wlepnu_Np0->Clone()
-#   Wlepnu->Add(Wlepnu_Np1)
-#   Wlepnu->Add(Wlepnu_Np2)
-#   Wlepnu->Add(Wlepnu_Np3)
-#   Wlepnu->Add(Wlepnu_Np4)
-#   Wlepnu->Add(Wlepnu_Np5)
+        Wtaunu = Wtaunu_Np0.Clone()
+        Wtaunu.Add(Wtaunu_Np1)
+        Wtaunu.Add(Wtaunu_Np2)
+        Wtaunu.Add(Wtaunu_Np3)
+        Wtaunu.Add(Wtaunu_Np4)
+        Wtaunu.Add(Wtaunu_Np5)
 
-#   TH1F* Wmunu = Wmunu_Np0->Clone()
-#   Wmunu->Add(Wmunu_Np1)
-#   Wmunu->Add(Wmunu_Np2)
-#   Wmunu->Add(Wmunu_Np3)
-#   Wmunu->Add(Wmunu_Np4)
-#   Wmunu->Add(Wmunu_Np5)
+        Wjets = Wlepnu.Clone()
+        Wjets.Add(Wtaunu)
 
-#   TH1F* Wtaunu = Wtaunu_Np0->Clone()
-#   Wtaunu->Add(Wtaunu_Np1)
-#   Wtaunu->Add(Wtaunu_Np2)
-#   Wtaunu->Add(Wtaunu_Np3)
-#   Wtaunu->Add(Wtaunu_Np4)
-#   Wtaunu->Add(Wtaunu_Np5)
+        Wgamma = Wgamma_Np0.Clone()
+        Wgamma.Add(Wgamma_Np1)
+        Wgamma.Add(Wgamma_Np2)
+        Wgamma.Add(Wgamma_Np3)
+        Wgamma.Add(Wgamma_Np4)
+        Wgamma.Add(Wgamma_Np5)
+        
+        st = st_tchan_lepnu.Clone()
+        st.Add(st_tchan_taunu)
+        st.Add(st_schan_lepnu)
+        st.Add(st_schan_taunu)
+        st.Add(st_Wt)
 
-#   TH1F* Wjets = Wlepnu->Clone()
-#   Wjets->Add(Wmunu)
-#   Wjets->Add(Wtaunu)
+        hn = ttbar.GetName()
+        c_paper = ROOT.TCanvas(hn +"_canvas", hn+"_canvas",700,410,500,400)
+        # c_paper.SetLogy()
 
-#   TH1F* Wgamma = Wgamma_Np0->Clone()
-#   Wgamma->Add(Wgamma_Np1)
-#   Wgamma->Add(Wgamma_Np2)
-#   Wgamma->Add(Wgamma_Np3)
-#   Wgamma->Add(Wgamma_Np4)
-#   Wgamma->Add(Wgamma_Np5)
+        bg = ROOT.THStack(hn+"_bg","stacked bg;"+ttbar.GetXaxis().GetTitle()+";Events")
 
-#   TH1F* st_tchan = st_tchan_lepnu->Clone()
-#   st_tchan->Add(st_tchan_munu)
-#   st_tchan->Add(st_tchan_taunu)
+        Wjets.SetFillStyle(1001)
+        Wgamma.SetFillStyle(1001)
+        ttbar.SetFillStyle(1001)
+        st.SetFillStyle(1001)
 
-#   TH1F* st_schan = st_schan_lepnu->Clone()
-#   st_schan->Add(st_schan_munu)
-#   st_schan->Add(st_schan_taunu)
-
-#   TH1F* st = st_tchan->Clone()
-#   st->Add(st_schan)
-#   st->Add(st_Wt)
-
-#   c_paper = new TCanvas("Paper","Paper",700,410,500,400)
-#   //c_paper->SetLogy()
-
-#   THStack *bg = new THStack("bg","stacked bgE_{T}^{miss} [GeV]Events")
-
-#   //bg->SetXTitle("E_{T}^{miss} [GeV]") 
-
-#   Wjets->SetFillStyle(1001)
-#   Wgamma->SetFillStyle(1001)
-#   ttbar->SetFillStyle(1001)
-#   st->SetFillStyle(1001)
-
-#   Wjets->SetFillColor(3)
-#   Wjets->SetLineColor(3)
-#   Wgamma->SetFillColor(7)
-#   Wgamma->SetLineColor(7)
-#   ttbar->SetFillColor(2)
-#   ttbar->SetLineColor(2)
-#   st->SetFillColor(9)
-#   st->SetLineColor(9)
+        Wjets.SetFillColor(3)
+        Wjets.SetLineColor(3)
+        Wgamma.SetFillColor(7)
+        Wgamma.SetLineColor(7)
+        ttbar.SetFillColor(2)
+        ttbar.SetLineColor(2)
+        st.SetFillColor(9)
+        st.SetLineColor(9)
   
-#   bg->Add(Wjets)
-#   bg->Add(Wgamma)
-#   bg->Add(ttbar)
-#   bg->Add(st)
+        bg.Add(Wjets)
+        bg.Add(Wgamma)
+        bg.Add(ttbar)
+        bg.Add(st)
+        
+        bg.Draw()
+        
+        legb = ROOT.TLegend(0.5,0.55,0.93,0.92)
+        legb.SetFillColor(0)
+        legb.SetBorderSize(0)
+        legb.SetTextSize(0.045)
+        legb.AddEntry(Wjets,"W+jets","f")
+        legb.AddEntry(Wgamma,"Wgamma","f")
+        legb.AddEntry(ttbar,"ttbar","f")
+        legb.AddEntry(st,"singe top","f")
+        legb.Draw()
 
-#   bg->Draw()
-
-#   legb = new TLegend(0.5,0.55,0.93,0.92)
-#   legb->SetFillColor(0)
-#   legb->SetBorderSize(0)
-#   legb->SetTextSize(0.045)
-#   legb->AddEntry(Wjets,"W+jets","f")
-#   legb->AddEntry(Wgamma,"Wgamma","f")
-#   legb->AddEntry(ttbar,"ttbar","f")
-#   legb->AddEntry(st,"singe top","f")
-#   legb->Draw()
-
-#   c_paper->Print("MetPlot.eps")
-#   c_paper->Print("MetPlot.png")
+        c_paper.Print(hn+"Plot.eps")
+        c_paper.Print(hn+"Plot.png")
   
-# }
 
     
