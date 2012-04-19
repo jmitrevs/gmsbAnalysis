5505dbbc43f1f13084a723c9bba437ecb52ed0d1
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2012-04-19 13:56:45 +0000

    add a few more files

---
+++ run/PathenaSignalGammaElectron_D2AOD.sh
@@ -0,0 +1,4 @@
+#
+
+pathena --mergeOutput --nGBPerJob=MAX --inDsTxt=D2AOD_DIPHO.list --outDS user.jmitrevs.120221.out_SignalGammaElectron SignalGammaElectron_Data.py 
+pathena --mergeOutput --nGBPerJob=MAX --inDsTxt=D2AOD_DIPHO.list --outDS user.jmitrevs.120221.out_SignalGammaElectron_ee SignalGammaElectron_Data_ee.py 
---
+++ run/PathenaSignalGammaElectron_data.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import os
+import commands
+import sys
+sfg="date '+%y%m%d%H%M'"
+Dates=os.system(sfg)
+
+# Put the list of datasets you want to run over here (remove the '/' from the end). 
+
+# this is for testing
+inDS_test = [
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodB.physics_Egamma.PhysCont.AOD.pro10_v01'
+
+    ]
+
+inDS_full = [
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodM.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodL.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodK.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodJ.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodI.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodH.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodG.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodF.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodE.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodD.physics_Egamma.PhysCont.AOD.pro10_v01',
+    'user.jmitrevs.eLgSkim_120221.data11_7TeV.periodB.physics_Egamma.PhysCont.AOD.pro10_v01',
+    ]
+#how to automatically configure GRL ?
+
+
+    
+for i,inDS in enumerate(inDS_full):
+
+    command = "pathena --mergeOutput --nGBPerJob=MAX --inDS %s/ --outDS user.jmitrevs.120419.%s SignalGammaElectron_Data.py " % (inDS, inDS)
+    
+    if i == 0:
+        command += " --outTarBall=/data3/jmitrevs/submission_gammael_data_120419.tar"
+    else:
+        command += " --inTarBall=/data3/jmitrevs/submission_gammael_data_120419.tar"
+
+    print command
+    sys.stdout.flush()
+    os.system(command)
+
+    command = "pathena --mergeOutput --nGBPerJob=MAX --inDS %s/ --outDS user.jmitrevs.120419_ej.%s SignalGammaElectron_Data_gjets.py " % (inDS, inDS)
+    
+    # if i == 0:
+    #     command += " --outTarBall=/data3/jmitrevs/submission_gammajet_data_120226.tar"
+    # else:
+    #     command += " --inTarBall=/data3/jmitrevs/submission_gammajet_data_120226.tar"
+
+    command += " --inTarBall=/data3/jmitrevs/submission_gammael_data_120419.tar"
+
+    print command
+    sys.stdout.flush()
+    os.system(command)
+    
