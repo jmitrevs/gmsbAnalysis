b0cd21365d20205bc46fdbc562ffa1246ad4b23f
Jovan Mitrevski <jovan.mitrevski@cern.ch>
2013-02-18 11:10:09 +0000

    remove some older algorithms

--- src/components/gmsbAnalysis_entries.cxx
+++ src/components/gmsbAnalysis_entries.cxx
@@ -1,5 +1,3 @@
-#include "gmsbAnalysis/BackgroundModelEE.h"
-#include "gmsbAnalysis/SignalGammaGamma.h"
 #include "gmsbAnalysis/SignalGammaLepton.h"
 #include "gmsbAnalysis/TriggerStudies.h"
 #include "gmsbAnalysis/PhotonEfficiency.h"
@@ -8,20 +6,14 @@
 #include "gmsbAnalysis/PileupHelper.h"
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
-DECLARE_ALGORITHM_FACTORY( BackgroundModelEE )
-DECLARE_ALGORITHM_FACTORY( SignalGammaGamma )
 DECLARE_ALGORITHM_FACTORY( SignalGammaLepton )
-DECLARE_ALGORITHM_FACTORY( TriggerStudies )
 DECLARE_ALGORITHM_FACTORY( PhotonEfficiency )
 DECLARE_ALGORITHM_FACTORY( PhotonIso )
 DECLARE_ALGORITHM_FACTORY( CutFlowHelper )
 DECLARE_ALGORITHM_FACTORY( PileupHelper )
 
 DECLARE_FACTORY_ENTRIES(gmsbAnalysis) {
-  DECLARE_ALGORITHM( BackgroundModelEE )
-  DECLARE_ALGORITHM( SignalGammaGamma )
   DECLARE_ALGORITHM( SignalGammaLepton )
-  DECLARE_ALGORITHM( TriggerStudies )
   DECLARE_ALGORITHM( PhotonEfficiency )
   DECLARE_ALGORITHM( PhotonIso )
   DECLARE_ALGORITHM( CutFlowHelper )
